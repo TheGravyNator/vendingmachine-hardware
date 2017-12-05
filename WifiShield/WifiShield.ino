0456f27b7f1878955a9ea042acc438b8ad46e1b1
“Christian <christian@23g.nl>
2017-12-05 10:34:58 +0100

    Added wifiConnect function

--- WifiShield/WifiShield.ino
+++ WifiShield/WifiShield.ino
@@ -8,6 +8,7 @@ class WifiConnection
 {
   public:
   WifiConnection(char* ssid, char* password);
+  void wifiConnect();
   char* _wifissid; //The SSID of the network
   char* _wifipassword; //The password of the network
 WifiConnection::WifiConnection(char* ssid, char* password)
@@ -15,6 +16,26 @@ WifiConnection::WifiConnection(char* ssid, char* password)
   _wifissid = ssid;
   _wifipassword = password;
 }
+void WifiConnection::wifiConnect()
+{
+  WiFi.disconnect();
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(_wifissid, _wifipassword);
+  delay(1000);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(_wifissid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
   private:
 };
 
