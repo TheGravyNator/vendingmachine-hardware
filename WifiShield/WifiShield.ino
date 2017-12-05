664aaad8ee10e5cd279c3704adf2b01ce6ae9816
“Christian <christian@23g.nl>
2017-12-05 13:44:47 +0100

    Added comments for includes

--- WifiShield/WifiShield.ino
+++ WifiShield/WifiShield.ino
@@ -7,11 +7,14 @@
  *  @date: December 5th, 2017
  **/
 
+//External libraries
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 #include <SoftwareSerial.h>
+
+//Internal header files
 #include "Secret.h"
 
 class WifiServer
