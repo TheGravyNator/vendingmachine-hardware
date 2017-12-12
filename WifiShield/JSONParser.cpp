3cccb544696eb6b7a2304d9d66cfd24067f0701b
“Christian <christian@23g.nl>
2017-12-12 15:41:07 +0100

    Added JSONParser class

---
+++ WifiShield/JSONParser.cpp
@@ -0,0 +1,5 @@
+#include "Arduino.h"
+#include "JSONParser.h"
+#include "SodaRequest.h"
+#include <ArduinoJson.h>
+
---
+++ WifiShield/JSONParser.h
@@ -0,0 +1,13 @@
+#ifndef JSONParser_h
+#define JSONParser_h
+
+#include "Arduino.h"
+#include "SodaRequest.h"
+#include <ArduinoJson.h>
+
+class JSONParser
+{
+  public: 
+};
+
+#endif
