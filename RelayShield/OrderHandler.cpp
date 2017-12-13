9a34ac8aaa31937a1ff4d2b8c4ace7b06241c157
“Christian <christian@23g.nl>
2017-12-13 17:37:14 +0100

    Added includes to OrderHandler class

---
+++ RelayShield/OrderHandler.cpp
@@ -0,0 +1,7 @@
+#include "Arduino.h"
+#include "OrderHandler.h"
+#include "Relay.h"
+#include "RelayHiLo.h"
+#include "SodaRequest.h"
+#include "Constants.h"
+
--- RelayShield/OrderHandler.h
+++ RelayShield/OrderHandler.h
@@ -1,6 +1,11 @@
 #ifndef OrderHandler_h
 #define OrderHandler_h
 
+#include "Arduino.h"
+#include "Relay.h"
+#include "RelayHiLo.h"
+#include "SodaRequest.h"
+
 class OrderHandler
 {
   public:
