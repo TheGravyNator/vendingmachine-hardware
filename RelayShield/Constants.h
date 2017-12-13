1b5fcc284df4c9a77e37893f0d0f6798254e89dc
“Christian <christian@23g.nl>
2017-12-13 17:35:47 +0100

    Defined constants through sketch

---
+++ RelayShield/Constants.h
@@ -0,0 +1,13 @@
+#define RELAY1_HI 4
+#define RELAY1_LO 5
+#define RELAY2_HI 7
+#define RELAY2_LO 6
+#define RELAY3_HI 8
+#define RELAY3_LO 9
+#define RELAY4_HI 12
+#define RELAY4_LO 13
+#define RELAY5 3
+#define DELAY_AMOUNT 2500
+
+#define RX_SERIAL 10
+#define TX_SERIAL 11
