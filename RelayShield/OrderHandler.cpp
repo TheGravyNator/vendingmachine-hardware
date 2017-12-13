e89051a23d50412115c677e08bb379c57e18b05e
“Christian <christian@23g.nl>
2017-12-13 17:38:48 +0100

    Added initialisation function to OrderClass

--- RelayShield/OrderHandler.cpp
+++ RelayShield/OrderHandler.cpp
@@ -5,3 +5,12 @@
 #include "SodaRequest.h"
 #include "Constants.h"
 
+void OrderHandler::init(RelayHiLo* relay1, RelayHiLo* relay2, RelayHiLo* relay3, RelayHiLo* relay4, Relay* relay5)
+{
+  _relay1 = relay1;
+  _relay2 = relay2;
+  _relay3 = relay3;
+  _relay4 = relay4;
+  _relay5 = relay5;
+}
+
