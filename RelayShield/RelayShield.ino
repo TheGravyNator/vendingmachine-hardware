b1c558bdcd3ce884a90ed23f39477647640c9581
“Christian <christian@23g.nl>
2017-12-13 17:32:41 +0100

    Added sketch description to RelayShield.ino

--- RelayShield/RelayShield.ino
+++ RelayShield/RelayShield.ino
@@ -1,3 +1,12 @@
+ /**
+ *  Name: RelayShield.ino
+ *  Purpose: The main code for the Relay shield of the 23G vending machine project.
+ *  It receives input from the WifiShield and controls the vending machine. 
+ *
+ *  @author: Christian Slabbekoorn
+ *  @date: December 13th, 2017
+ **/
+
 void setup() 
 {
   
